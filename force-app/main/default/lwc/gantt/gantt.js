2b8bfe8d1f334a37945725c1df0d0730340bf8fa
Chidambar Reddy <chidambar.reddy@daddydonkeylabs.com>
2022-05-24 00:37:01 +0530

    Group By Projects - Gantt

--- force-app/main/default/lwc/gantt/gantt.js
+++ force-app/main/default/lwc/gantt/gantt.js
@@ -12,16 +12,17 @@ import GanttFiles from '@salesforce/resourceUrl/dhtmlxgantt713';
 import getTasks from '@salesforce/apex/GanttData.getTasks';
 import createResource from '@salesforce/apex/GanttData.createResource';
 import getResourcesByProject from '@salesforce/apex/GanttData.getResourcesByProject';
+import getAllResourcesByProject from '@salesforce/apex/GanttData.getAllResourcesByProject';
 
 function unwrap(fromSF){
     const data = fromSF.tasks.map(a => ({
-        id: a.Id,
-        text: a.Resource__r.Name,
-        start_date: a.Start_Date_of_Engagement__c,
-        end_date: a.End_Date_of_Engagement__c,
+        id: a.identifier,
+        text: a.displayLabel,
+        start_date: a.startDate,
+        end_date: a.endDate,
         //duration: a.Number_of_Weekdays__c,
-        parent: '',
-        progress: a.Progress__c,
+        parent: a.parentIdentifier,
+        progress: a.progress,
     }));
     const links = fromSF.links.map(a => ({
         id: a.Id,
@@ -74,16 +75,26 @@ export default class GanttView extends LightningElement {
         ];
 
         gantt.config.columns =  [
-            {name:"text",       label:"Resource",  tree:true },
-            {name:"start_date", label:"Start Date", align:"center" },
-            {name:"end_date",   label:"End Date",   align:"center" }
+            {name:"text",       label:"Resource",  tree:true , autoWidth: true},
+            {name:"start_date", label:"Start Date", align:"center", autoWidth: true},
+            {name:"end_date",   label:"End Date",   align:"center", autoWidth: true}
         ];
+        gantt.config.open_tree_initially = true;
+
         gantt.templates.parse_date = date => new Date(date);
         gantt.templates.format_date = date => date.toISOString();
+        
         gantt.init(root);
-        getResourcesByProject({currentRecordId:this.recordId}).then(d => {
-            gantt.parse(unwrap(d));
-        })
+        if(this.recordId){
+            getResourcesByProject({currentRecordId:this.recordId}).then(d => {
+                gantt.parse(unwrap(d));
+            })
+        }
+        else{
+            getAllResourcesByProject({}).then(d => {
+                gantt.parse(unwrap(d));
+            })
+        }
 
         gantt.createDataProcessor({
             task: {
@@ -110,11 +121,14 @@ export default class GanttView extends LightningElement {
 
                 },
                 update: function(data, id) {
+                    console.log(data);
                     const update = { fields:{
                         Id: id,
                         //Name : data.text,
+                        // Start_Date_of_Engagement__c : new Date(data.start_date?.getFullYear(),data.start_date?.getMonth(),data.start_date?.getDate()),
                         Start_Date_of_Engagement__c : data.start_date,
-                        End_Date_of_Engagement__c : data.end_date
+                        // End_Date_of_Engagement__c :new Date(data.end_date?.getFullYear(),data.end_date?.getMonth(),data.end_date?.getDate())
+                        End_Date_of_Engagement__c :data.end_date
                         //Duration__c : data.duration,
                         //Parent__c : String(data.parent),
                         //Progress__c : data.progress
