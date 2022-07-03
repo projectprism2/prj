744a33470e283326d34bacd800288bb9cd250097
Vikas Patil <vikas@formulaq.io>
2022-07-03 22:37:52 +0530

    Update records in Gate-path

--- force-app/main/default/lwc/gatePath/gatePath.js
+++ force-app/main/default/lwc/gatePath/gatePath.js
@@ -1,15 +1,16 @@
-import { LightningElement, wire,api,track } from 'lwc';
+import { LightningElement, wire,api } from 'lwc';
 import getAssignedGatesByProject from '@salesforce/apex/gate.getAssignedGatesByProject';
+import updateAssignedGatesByProject from '@salesforce/apex/gate.updateAssignedGatesByProject';
 
 export default class GatePath extends LightningElement {
-    @track assignedGates=[];
+    gates;
+    assignedGates=[];
     @api recordId;
 
     @wire(getAssignedGatesByProject,{projectId:'$recordId'}) 
     wiredAssignedGates({error,data}){
         if(data){
-            console.log(data);
-            console.log('assignedGates');
+            this.gates = data;
             this.assignedGates = data.map(gate =>{
                 if(gate.Status__c == 'Open'){
                     return {Id:gate.Id,Name:gate.Name,Status:gate.Status__c,gateStatus:'slds-path__item slds-is-incomplete'};
@@ -27,5 +28,33 @@ export default class GatePath extends LightningElement {
         if(error){
             console.log(error);
         }
+    }
+
+    updateAssignedGatesByProject(){
+
+        let gateId = 'a011y000003D3KCAA0';
+        updateAssignedGatesByProject({projectId:this.recordId,assignedGateId:gateId})
+        .then(result =>{
+            getRecordNotifyChange([{recordId: this.recordId}]);
+            console.log('succesfully updated path');
+        })
+        .catch(error =>{
+            console.log(error);
+        })
+
+        /*
+        let breakLoop = false;
+        console.log(this.gates);
+        const fields = this.gates.map(gate =>{
+            if(!breakLoop){
+                if(gate.Id == assignedGateId){
+                    breakLoop = true;
+                    return {Id:gate.Id,Name:gate.Name,Status__c:'In Progress',Project__c:gate.Project__c};
+                }
+                return {Id:gate.Id,Name:gate.Name,Status__c:'Completed',Project__c:gate.Project__c};
+            }
+            return;
+        });
+        */
     }
 }
